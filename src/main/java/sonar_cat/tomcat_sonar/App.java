c8da4a9f9bc8b3b994214a6bd9b0f7790edb9b7b
prasa <prasa@LAPTOP-H0PSVFQQ>
2022-11-27 15:31:17 +0530

    no msg

---
+++ src/main/java/sonar_cat/tomcat_sonar/App.java
@@ -0,0 +1,13 @@
+package sonar_cat.tomcat_sonar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
